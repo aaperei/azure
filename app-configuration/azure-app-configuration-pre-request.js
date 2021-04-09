6b34d19317990c1de1c40f5cc551171934eec984
aaperei <alisonamp.eco@gmail.com>
2021-04-09 10:43:27 -0300

    Azure App Configuration

---
+++ app-configuration/azure-app-configuration-pre-request.js
@@ -0,0 +1,52 @@
+var credential = "YOUR-APP-CONFIGURATION-CREDENTIAL-ID";
+var secret = "YOUR-APP-CONFIGURATION-CREDENTIAL-VALUE";
+
+function signRequest(host, 
+                     method,      // GET, PUT, POST, DELETE
+                     url,         // path+query
+                     body,        // request body (undefined of none)
+                     credential,  // access key id
+                     secret)      // access key value (base64 encoded)
+{
+        var verb = method.toUpperCase();
+        var utcNow = new Date().toUTCString();
+        var contentHash = CryptoJS.SHA256(body).toString(CryptoJS.enc.Base64);
+
+        //
+        // SignedHeaders
+        var signedHeaders = "x-ms-date;host;x-ms-content-sha256"; // Semicolon separated header names
+
+        //
+        // String-To-Sign
+        var stringToSign = 
+            verb + '\n' +                              // VERB
+            url + '\n' +                               // path_and_query
+            utcNow + ';' + host + ';' + contentHash;   // Semicolon separated SignedHeaders values
+
+        //
+        // Signature
+        var signature = CryptoJS.HmacSHA256(stringToSign, CryptoJS.enc.Base64.parse(secret)).toString(CryptoJS.enc.Base64);
+
+        //
+        // Result request headers
+        return [
+            { name: "x-ms-date", value: utcNow },
+            { name: "x-ms-content-sha256", value: contentHash },
+            { name: "Authorization", value: "HMAC-SHA256 Credential=" + credential + "&SignedHeaders=" + signedHeaders + "&Signature=" + signature }
+        ];
+}
+
+var isBodyEmpty = pm.request.body === null || pm.request.body === undefined || pm.request.body.isEmpty();
+
+var headers = signRequest(
+    pm.request.url.getHost(),
+    pm.request.method,
+    pm.request.url.getPathWithQuery(),
+    isBodyEmpty ? undefined : pm.request.body.toString(),
+    credential,
+    secret);
+
+// Add headers to the request
+headers.forEach(header => {
+    pm.request.headers.upsert({key: header.name, value: header.value});
+})
